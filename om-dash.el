aa2c5abcd50773b168119a83838e4d35828d9ab0
Victor Gaydov <victor@enise.org>
2025-03-11 17:48:21 +0900

    Update keywords

--- om-dash.el
+++ om-dash.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/gavv/om-dash
 ;; Version: 0.6
 ;; Package-Requires: ((emacs "28.2") (org-ql "0.8.10") (s "1.13.0") (ts "0.3"))
-;; Keywords: org outlines
+;; Keywords: org, outlines
 
 ;;; License:
 
